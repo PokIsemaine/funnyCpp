d46e14f82815f993122f5c5f141048708f7167df
fishingrod <851892190@qq.com>
2022-07-09 21:25:21 +0800

    play future

--- mordencpp/Ch7/mutex/future/future.cpp
+++ mordencpp/Ch7/mutex/future/future.cpp
@@ -2,7 +2,27 @@
 #include <future>
 #include <iostream>
 
+int sum1 (int a, int b) {
+	return a + b;
+}
+int sum2 (int a, int b, int c) {
+	return a + b + c;
+}
+
 int main() {
+
+	std::packaged_task<int(int,int)> task1(sum1);
+
+	std::future<int> res = task1.get_future();
+
+	std::thread t(std::move(task1), 10 , 20);
+	t.detach();
+	
+	// 得到结果之前会阻塞
+	std::cout << res.get() << std::endl;
+
+#if 0
+
 	std::packaged_task<int()> task([](){ return 7;});
 	
 	// 获得 task 的期物
@@ -16,5 +36,6 @@ int main() {
 				
 	std::cout << "done!" << std::endl << "future result is " << result.get() << std::endl;
 
+#endif
 	return 0;
 }
